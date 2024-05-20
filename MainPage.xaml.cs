30e11df6d413a13ef91f1192c04cf7101c745f0d
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-05-20 14:25:56 +0000

    Dodaj pliki projektów.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace StylingTextMaui
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void Switch_Toggled_1(object sender, ToggledEventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StylingTextMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
