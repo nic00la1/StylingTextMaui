a0c25d1d93384692abdfa664d1ab11361565ada8
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-05-20 15:00:14 +0000

    Projekt dziala, oprocz pozycji tekstu ✅

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,25 +2,31 @@
 {
     public partial class MainPage : ContentPage
     {
+        string[] quotes = new string[5] { "Cytat 1", "Cytat 2", "Cytat 3", "Cytat 4", "Cytat 5" };
+        Random random = new Random();
 
         public MainPage()
         {
             InitializeComponent();
+            slider.ValueChanged += Slider_ValueChanged;
+            button.Clicked += button_Clicked;
         }
 
-        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        void Slider_ValueChanged(object? sender, ValueChangedEventArgs e)
         {
-
+            sizeLabel.Text = "Rozmiar: " + e.NewValue;
         }
 
-        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        void button_Clicked(object? sender, EventArgs e)
         {
-
-        }
-
-        private void Switch_Toggled_1(object sender, ToggledEventArgs e)
-        {
-
+            int index = random.Next(quotes.Length);
+            quoteLabel.Text = quotes[index];
+            quoteLabel.FontSize = slider.Value;
+            if (picker.SelectedItem != null)
+            {
+                quoteLabel.HorizontalOptions = (LayoutOptions)Enum.Parse(typeof(LayoutOptions), picker.SelectedItem.ToString());
+            }
+            quoteLabel.FontAttributes = (boldSwitch.IsToggled ? FontAttributes.Bold : FontAttributes.None) | (italicSwitch.IsToggled ? FontAttributes.Italic : FontAttributes.None);
         }
     }
 
